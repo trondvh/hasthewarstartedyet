cbc21cce99a697d822739383b3b34a35eb9877cd
trondvh <trond.hapnes@gmail.com>
2022-02-17 18:17:47 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Has the war startet yet?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Nope" />
       </main>
 
-      <Footer />
     </div>
   )
 }
